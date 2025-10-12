92ff514f3d7917f35e95fc2f26e4a5c6f5bafcb8
Filip Biterski <filip.biterski@filibit.com>
2025-10-12 20:39:44 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONFIG_PATH=${XDG_CONFIG_HOME:-"$HOME/.config"}
+
+brew install koekeishiya/formulae/skhd
+
+mkdir $CONFIG_PATH/skhd
+cp ./skhd/.skhdrc $CONFIG_PATH/skhd/.skhdrc
+
+brew install --cask amethyst
+
+defaults write com.apple.dock "mru-spaces" -bool "false" && killall Dock
