95bbeb378a61173e5bec65cc9f5ea96003fa2d41
Filip Biterski <filip.biterski@filibit.com>
2025-10-17 22:03:32 +0200

    add amethyst configuration file

--- setup.sh
+++ setup.sh
@@ -5,8 +5,13 @@ CONFIG_PATH=${XDG_CONFIG_HOME:-"$HOME/.config"}
 brew install koekeishiya/formulae/skhd
 
 mkdir $CONFIG_PATH/skhd
-cp ./skhd/.skhdrc $CONFIG_PATH/skhd/.skhdrc
+cp ./skhd/skhdrc $CONFIG_PATH/skhd/skhdrc
 
 brew install --cask amethyst
 
 defaults write com.apple.dock "mru-spaces" -bool "false" && killall Dock
+
+mkdir $CONFIG_PATH/amethyst
+cp ./amethyst/amethyst.yml $CONFIG_PATH/amethyst/amethyst.yml
+
+
